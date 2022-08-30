e936159af853bf502cfee3fec12c3c7a46002606
lec89 <lucascorts@hotmail.com>
2022-08-30 15:54:10 -0300

    get, patch and delete added

--- routes/cities.js
+++ routes/cities.js
@@ -1,9 +1,11 @@
 var express = require('express')
-var router =  express.Router()
+var router = express.Router()
 const {create, read, update, destroy} = require('../controllers/cityController')
 
 // GET users listing
 router.post('/', create)
 router.get('/:id', read)
+router.patch('/:id', update)
+router.delete('/:id', destroy)
 
 module.exports = router
