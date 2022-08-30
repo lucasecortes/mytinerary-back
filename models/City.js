8d90c9275a6e2114845d558d2712f11cabfc8b5a
lec89 <lucascorts@hotmail.com>
2022-08-30 12:22:28 -0300

    City.js modified

--- models/City.js
+++ models/City.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose')
 
-const schema = new mongoose.Schema({
+const citiesSchema = new mongoose.Schema({
     city: {type: String, required: true},
     country: {type: String, required: true},
     photo: {type: String, required: true},
     population: {type: Number, required: true},
-    fundation: {type: Date, require: true},
+    fundation: {type: Date, required: true},
 })
 
 const City = mongoose.model(
     'cities',
-    schema
+    citiesSchema
 )
 
 module.exports = City
