0d3343080bffbbbcf688c716010cea9758455def
adrieldussex <adrieldussex@gmail.com>
2022-09-07 02:57:26 -0300

    add router get/all

--- routes/itinerary.js
+++ routes/itinerary.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const {create, update, destroy} = require('../controllers/itineraryController')
+const {create, update, destroy, all} = require('../controllers/itineraryController')
 
 router.post('/', create);
+router.get('/', all)
 router.patch('/:id', update)
 router.delete('/:id', destroy)
 
