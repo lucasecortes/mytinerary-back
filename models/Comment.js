5d98777e4fa7646c7eb8f83b40225fa1a441126e
adrieldussex <adrieldussex@gmail.com>
2022-09-07 14:01:14 -0300

    Modified comment model

--- models/Comment.js
+++ models/Comment.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 const commentsSchema = new mongoose.Schema({
     comment: {type: String, required: true},
-    user: {type: String},
-    itinerary: {type: String},
+    user: {type: mongoose.Types.ObjectId, ref: 'users', required: true},
+    itinerary: {type: mongoose.Types.ObjectId, ref: 'itineraries', required: true},
 })
 
 const Comment = mongoose.model(
