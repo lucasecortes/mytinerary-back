a07409360220977d5cb6a19774d326a9039158e4
adrieldussex <adrieldussex@gmail.com>
2022-09-12 17:51:42 -0300

    add routes: signin, signup and verify

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const {create, all} = require('../controllers/userController')
+const {create, all, verifyMail, signUp, signIn} = require('../controllers/userController')
 
 /* GET users listing. */
 router.post('/', create);
 router.get('/', all);
+router.post('/signup', signUp)
+router.post('/signin', signIn)
+router.get('/verify/:code', verifyMail)
 
 module.exports = router;
