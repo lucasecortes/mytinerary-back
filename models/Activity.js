2b269db648280c818cd0f6978f5cd10d4d1a4305
lec89 <lucascorts@hotmail.com>
2022-09-15 20:17:03 -0300

    modified

--- models/Activity.js
+++ models/Activity.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const activitiesSchema = new mongoose.Schema({
     name: {type: String, required: true},
     photo: {type: String, required: true},
-    itinerary: {type: mongoose.Types.ObjectId, ref:'itineraries'},
+    itinerary: {type: mongoose.Types.ObjectId, ref:'itineraries', required: true},
 })
 
 const Activity = mongoose.model(
