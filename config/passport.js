9ddbd3e77d3a779ef722379c4e7cb9d2fa64a5a3
adrieldussex <adrieldussex@gmail.com>
2022-09-20 16:33:08 -0300

    create passport

---
+++ config/passport.js
@@ -0,0 +1,39 @@
+const passport = require('passport')
+const passportJwt = require('passport-jwt')
+
+const {KEY_JWT} = process.env
+const User = require('../models/User')
+
+passport.use( 
+        // si se cumplen las condiciones del pasaporte, avanza al método del controlador
+    new passportJwt.Strategy( // definimos la forma en la que se va a decodificar y la clave de validación
+        {
+            jwtFromRequest: passportJwt.ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: KEY_JWT
+        }, // nos retorna un objeto jwt_payload(con la data del token)
+        async (jwt_payload,done) => {
+            console.log(jwt_payload)
+            try {
+                let user = await User.findOne({_id:jwt_payload.id})
+                console.log(user)
+                if (user) {
+                    user = {
+                        id: user._id,
+                        name: user.name,
+                        email: user.email,
+                        role: user.role,
+                        photo: user.photo,
+                    }
+                    return done(null, user)
+                } else {
+                    return done(null, false)
+                }
+            } catch (error) {
+                console.log(error)
+                return done(error,false)
+            }
+        } 
+    )
+)
+
+module.exports = passport
