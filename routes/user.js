26bb23be61b683b49afbdcab1c189770cf802b6f
lec89 <lucascorts@hotmail.com>
2022-09-21 21:04:07 +0200

    verifytoken added

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,14 @@
 var express = require('express');
+let passport = require('../config/passport');
 var router = express.Router();
-const {all, oneUser, verifyMail, signUp, signIn, signOut} = require('../controllers/userController')
+const {all, oneUser, verifyMail, verifyToken, signUp, signIn, signOut} = require('../controllers/userController')
 
 
 /* GET users listing. */
 router.get('/', all);
 router.get('/:id', oneUser);
 router.get('/signup', all);
+router.get('/token', passport.authenticate('jwt', {session:false}), verifyToken)
 router.post('/signup', signUp)
 router.post('/signout', signOut)
 router.post('/signin', signIn)
