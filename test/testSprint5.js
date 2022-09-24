59f80a8cc480c2cbf6cc3a9a0232185b16b61a46
lec89 <lucascorts@hotmail.com>
2022-09-24 17:06:17 +0200

    test done

--- test/testSprint5.js
+++ test/testSprint5.js
@@ -1,29 +1,34 @@
 const request = require('supertest')
 const app = require('../server')
 const { assert } = require('chai')
-let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMmRlY2EyNzQ4Y2M0ODcwY2FkY2E3NSIsImlhdCI6MTY2Mzk1NDEyMywiZXhwIjoxNjY0MDQwNTIzfQ.3ljgs1YJg2c08o2SH4hqHKi_EPyxsjEUBrz5d3WRNXY'
+let token = ''
 
-// describe('POST /events', function () {
-//     it('Must respond with 201 status code', function (done) {
-//         request(app)
-//             .post('/events')
-//             .send({})
-//             .expect(201)
-//             .end(function (err, res) {
-//                 if (err) return done(err);
-//                 return done()
-//             })
-//     })
-// })
+describe('POST /auth/signin', function () {
+    it('Must respond with 201 status code', function () {
+        request(app)
+            .post('/auth/signin')
+            .send({
+                mail: "liomessi10@gmail.com",
+                password: "Scaloneta123",
+                from: 'form'
+            })
+            .expect(201)
+            .then(response => {
+                token = response.token
+                assert.isString(response.body.name)
+            })
+    })
+})
 
 describe('PATCH /itineraries/likes/:id', function () {
     it('Must respond with the following like has been updated', function () {
         request(app)
             .patch('/itineraries/likes/63239816ec4de05815f7e790')
-            .send({
-                headers: {Authorization: 'Bearer ' + token}
+            .send({})
+            .set({
+                "Authorization": 'Bearer ' + token
             })
-            .expect(401)
+            .expect(200)
             .then((res) => {
                 assert.isObject(res.body.id)
                 done()
@@ -35,6 +40,17 @@ describe('GET /itineraries/auth', function () {
     it('Must respond with 404 status code', function (done) {
         request(app)
             .get('/itineraries/auth')
+            .send({})
+            .expect(404)
+            .end((err, res) => {
+                if (err) return done(err)
+                return done()
+            })
+    })
+
+    it('Must respond with 404 status code', function (done) {
+        request(app)
+            .get('/itineraries/6318cce09520ca907a04cb27')
             .send({})
             .expect(404)
             .end((err, res) => {
